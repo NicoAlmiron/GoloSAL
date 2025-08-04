e00ab436f2cfa04eda8b62e01c22e7059c5b36d0
Nico Almiron <almiron.nicolas16@gmail.com>
2025-08-04 16:35:57 -0300

    Creacion de la Clase Cliente con sus parametros

---
+++ GoloSAL/Models/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GoloSAL.Models
+{
+    public class ClienteModel
+    {
+        public int IdContacto { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion {  get; set; }
+        public string? Telefono { get; set; }
+
+
+    }
+}
