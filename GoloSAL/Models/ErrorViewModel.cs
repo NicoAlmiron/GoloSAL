f6ef5fbbea1edbc317696ffc61453a552a133ab6
adria <adria@NICOAL>
2025-08-04 16:26:16 -0300

    Agregar archivos de proyecto.

---
+++ GoloSAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoloSAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
